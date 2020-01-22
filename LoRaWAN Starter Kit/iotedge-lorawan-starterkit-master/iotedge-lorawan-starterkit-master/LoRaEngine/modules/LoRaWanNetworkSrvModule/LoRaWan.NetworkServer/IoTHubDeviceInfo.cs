5a680a7e69305957bf0b262b236ff178294ddee3
Joel Morran <39391416+JoelMorran@users.noreply.github.com>
2020-01-22 15:03:18 +1100

    Add files via upload

---
+++ LoRaWAN Starter Kit/iotedge-lorawan-starterkit-master/iotedge-lorawan-starterkit-master/LoRaEngine/modules/LoRaWanNetworkSrvModule/LoRaWan.NetworkServer/IoTHubDeviceInfo.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace LoRaWan.NetworkServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class IoTHubDeviceInfo
+    {
+        public string DevAddr { get; set; }
+
+        public string DevEUI { get; set; }
+
+        public string PrimaryKey { get; set; }
+
+        public IoTHubDeviceInfo()
+        {
+        }
+
+        public IoTHubDeviceInfo(string devAddr, string devEUI, string primaryKey)
+        {
+            this.DevAddr = devAddr;
+            this.DevEUI = devEUI;
+            this.PrimaryKey = primaryKey;
+        }
+    }
+}
